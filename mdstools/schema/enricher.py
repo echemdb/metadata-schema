58cda4425714205f085e95bf3335e84cbc0e6ca8
Albert Engstfeld <albert.engstfeld@gmail.com>
2026-02-17 23:40:07 +0100

    Include license agreements

--- mdstools/schema/enricher.py
+++ mdstools/schema/enricher.py
@@ -1,5 +1,24 @@
 """Schema enricher for adding descriptions and examples from JSON Schema to flattened data."""
 
+# ********************************************************************
+#  This file is part of mdstools.
+#
+#        Copyright (C) 2026 Albert Engstfeld
+#
+#  mdstools is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  mdstools is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with mdstools. If not, see <https://www.gnu.org/licenses/>.
+# ********************************************************************
+
 import json
 import os
 from pathlib import Path
